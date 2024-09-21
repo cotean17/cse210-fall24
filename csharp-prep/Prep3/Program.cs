0b961d7d001eaf479b12f655ce645001dc905b75
mariajosemackay <mariajoarevalon@gmail.com>
2024-09-21 02:52:33 -0600

    Assihgnment Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Claims;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello! What is the magic number? ");
+        //string userInput = Console.ReadLine();
+        //int magicNumber = int.Parse(userInput)
+        
+        // Random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 200);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+            
+            if(guessNumber > magicNumber)
+            {
+                Console.WriteLine("a little bit lower"); 
+            }
+            if(guessNumber < magicNumber)
+            {
+                Console.WriteLine(" a little bit higher"); 
+            }
+            if(guessNumber == magicNumber)
+            {
+                Console.WriteLine("That's the one!"); 
+            }
+        }
+       
     }
 }
