703130097f13399dd63a98485e3c6be7d01a98b6
mariajosemackay <mariajoarevalon@gmail.com>
2024-09-21 01:50:23 -0600

    Assignment done prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your porcentage?");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+
+        string letter = "";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade this semester is: {letter}");
+
+        if (number >= 70)
+        {
+            Console.WriteLine("Congrats, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Maybe next semester!");
+        }
     }
 }
